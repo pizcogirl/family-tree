186ee58650f8616deba3fc5445798c02a632db73
Julia <julia.zuara.jimenez@gmail.com>
2015-01-07 17:59:32 +0100

    Añadidos atributos nuevos, metodo showData

--- Person.java
+++ Person.java
@@ -1,33 +1,42 @@
+import java.util.ArrayList;
 
 /**
- * Write a description of class Person here.
+ * Representa a una persona
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Person
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // Almacena el nombre de la persona
+    private String name;
+    // Almacena la edad de la persona
+    private int age;
+    // Almacena al padre de la persona
+    private Person father;
+    // Almacena a la madre de la persona
+    private Person mother;
+    // Almacena a todos los hijos de la persona
+    private ArrayList<Person> children;
 
     /**
-     * Constructor for objects of class Person
+     * Constructor para la persona. Introduce el nombre y la edad
      */
-    public Person()
+    public Person(String name, int age)
     {
-        // initialise instance variables
-        x = 0;
+        // Inicializa las variables mediante parametros
+        this.name = name;
+        this.age = age;
+    }
+    
+    /**
+     * Metodo que imprime por pantalla la informacion sobre la persona
+     */
+    public void showData()
+    {
+        // Imprime por pantalla los datos de la persona
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + age);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
